8b26d9f0b161ddbb9dc8d882edc87d6787c8df50
Zach-Haddad <Zhaddad7@gmail.com>
2017-05-16 13:12:50 -0700

    adoption helper refactor

--- testing/cucumber/exercises/two_puppies_refactor.rb
+++ testing/cucumber/exercises/two_puppies_refactor.rb
@@ -1,38 +1,6 @@
 require 'Watir'
-
-def goto_adoption_site
-  @browser = Watir::Browser.new :chrome
-  @browser.goto 'http://puppies.herokuapp.com'
-end
-
-def adopt_puppy(puppy_index)
-  @browser.button(value: 'View Details', index: puppy_index).click
-  @browser.button(value: 'Adopt Me!').click
-end
-
-def continue_adopting
-  @browser.button(value: 'Adopt Another Puppy').click
-end
-
-def finish_adopting
-  @browser.button(value: 'Complete the Adoption').click
-end
-
-def checkout(name, address, email, payment_method)
-  @browser.text_field(name: 'order[name]').set(name)
-  @browser.textarea(name: 'order[address]').set(address)
-  @browser.text_field(name: 'order[email]').set(email)
-  @browser.select_list(name: 'order[pay_type]').select(payment_method)
-  @browser.button(value: 'Place Order').click
-end
-
-def verify_page_contains(text, error = "There was an error")
-  fail error unless @browser.text.include? text
-end
-
-def close_browser
-  @browser.close
-end
+require_relative 'adoption_helper'
+include AdoptionHelper
 
 goto_adoption_site
 adopt_puppy(0)
