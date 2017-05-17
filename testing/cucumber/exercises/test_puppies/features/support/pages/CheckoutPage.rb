0920f40d68b24716d2a02959678860afb26abf30
Zach-Haddad <Zhaddad7@gmail.com>
2017-05-16 22:06:33 -0700

    refactor with PageObject gem

--- testing/cucumber/exercises/test_puppies/features/support/pages/CheckoutPage.rb
+++ testing/cucumber/exercises/test_puppies/features/support/pages/CheckoutPage.rb
@@ -1,25 +1,36 @@
 class CheckoutPage
-  def initialize(browser)
-    @browser = browser
-  end
+  # with PageObject gem
+  include PageObject
 
-  def address=(address)
-    @browser.textarea(name: 'order[address]').set(address)
-  end
+  text_field(:name, id: "order_name")
+  textarea(:address, id: "order_address")
+  text_field(:email, id: "order_email")
+  select_list(:pay_type, id: "order_pay_type")
+  button(:place_order, value: "Place Order")
 
-  def name=(name)
-    @browser.text_field(name: 'order[name]').set(name)
-  end
+  # without PageObject gem
 
-  def email=(email)
-    @browser.text_field(name: 'order[email]').set(email)
-  end
-
-  def pay_type=(pay_type)
-    @browser.select_list(name: 'order[pay_type]').select(pay_type)
-  end
-
-  def place_order
-    @browser.button(value: 'Place Order').click
-  end
+  # def initialize(browser)
+  #   @browser = browser
+  # end
+  #
+  # def address=(address)
+  #   @browser.textarea(name: 'order[address]').set(address)
+  # end
+  #
+  # def name=(name)
+  #   @browser.text_field(name: 'order[name]').set(name)
+  # end
+  #
+  # def email=(email)
+  #   @browser.text_field(name: 'order[email]').set(email)
+  # end
+  #
+  # def pay_type=(pay_type)
+  #   @browser.select_list(name: 'order[pay_type]').select(pay_type)
+  # end
+  #
+  # def place_order
+  #   @browser.button(value: 'Place Order').click
+  # end
 end
--- testing/cucumber/exercises/test_puppies/features/support/pages/ShoppingCartPage.rb
+++ testing/cucumber/exercises/test_puppies/features/support/pages/ShoppingCartPage.rb
@@ -3,39 +3,61 @@ class ShoppingCartPage
   SUBTOTAL_COLUMN = 3
   LINES_PER_PUPPY = 6
 
-  def initialize(browser)
-    @browser = browser
-  end
+  # with PageObject gem
+  include PageObject
 
+  button(:proceed_to_checkout, value: "Complete the Adoption")
+  button(:continue_shopping, value: "Adopt Another Puppy")
+  table(:cart, index: 0)
+  cell(:cart_total, class: "total_cell")
 
   def line_item_subtotal(line_item)
-    cart_line_item(line_item.to_i)[SUBTOTAL_COLUMN].text
+    table_value(line_item, SUBTOTAL_COLUMN)
   end
 
   def line_item_name(line_item)
-    cart_line_item(line_item.to_i)[NAME_COLUMN].text
+    table_value(line_item, NAME_COLUMN)
   end
 
-  def cart_total
-    @browser.td(class: 'total_cell').text
-  end
+  # without PageObject gem
 
-  def proceed_to_checkout
-    @browser.button(value: 'Complete the Adoption').click
-  end
+  # def initialize(browser)
+  #   @browser = browser
+  # end
 
-  def continue_shopping
-    @browser.button(value: 'Adopt Another Puppy').click
-  end
-
+  # def line_item_subtotal(line_item)
+  #   cart_line_item(line_item.to_i)[SUBTOTAL_COLUMN].text
+  # end
+  #
+  # def line_item_name(line_item)
+  #   cart_line_item(line_item.to_i)[NAME_COLUMN].text
+  # end
+  #
+  # def cart_total
+  #   @browser.td(class: 'total_cell').text
+  # end
+  #
+  # def proceed_to_checkout
+  #   @browser.button(value: 'Complete the Adoption').click
+  # end
+  #
+  # def continue_shopping
+  #   @browser.button(value: 'Adopt Another Puppy').click
+  # end
+  #
   private
 
-  def row_for(line_item)
-    (line_item - 1) * LINES_PER_PUPPY
+  def table_value(lineitem, column)
+    row = (lineitem.to_i - 1) * LINES_PER_PUPPY
+    cart_element[row][column].text
   end
 
-  def cart_line_item(line_item)
-    @browser.table(index: 0)[row_for(line_item)]
-  end
+  # def row_for(line_item)
+  #   (line_item - 1) * LINES_PER_PUPPY
+  # end
+  #
+  # def cart_line_item(line_item)
+  #   @browser.table(index: 0)[row_for(line_item)]
+  # end
 
 end
