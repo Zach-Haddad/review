bf8b809cc5754c15d9b0423ba23a8d082922aa8e
Zach-Haddad <Zhaddad7@gmail.com>
2017-07-28 22:48:52 -0700

    Start ch 6

---
+++ haskell/LYAH/ch6.hs
@@ -0,0 +1,8 @@
+-- Higher Order Functions
+
+-- Currying
+
+-- multThree :: (Num a) => a -> a -> a -> a
+-- multThree x y z = x * y * z
+-- Think of as:
+-- multThree :: (Num a) => a -> (a -> (a -> a))
