1d185240d740f17ee2a15fee962858a3c4be30a5
Zach-Haddad <Zhaddad7@gmail.com>
2017-07-31 20:32:15 -0700

    ch 6 II

--- haskell/LYAH/ch6.hs
+++ haskell/LYAH/ch6.hs
@@ -6,3 +6,37 @@
 -- multThree x y z = x * y * z
 -- Think of as:
 -- multThree :: (Num a) => a -> (a -> (a -> a))
+
+
+-- applyTwice :: (a -> a) -> a -> a
+-- applyTwice f x = f (f x)
+
+-- zipWith; part of std lib
+--
+-- zipWith' :: (a -> b -> c) -> [a] -> [b] -> [c]
+-- zipWith' _ [] _ = []
+-- zipWith' _ _ [] = []
+-- zipWith' f (x:xs) (y:ys) = f x y : zipWith' f xs ys
+
+
+-- Mapping and filtering
+
+-- map :: (a -> b) -> [a] -> [b]
+-- map _ [] = []
+-- map f (x:xs) = f x : map f xs
+
+-- filter :: (a -> Bool) -> [a] -> [a]
+-- filter _ [] = []
+-- filter p (x:xs)
+--     | p x       = x : filter p xs
+--     | otherwise = filter p xs
+
+
+-- quicksort as filter
+
+-- quicksort :: (Ord a) => [a] -> [a]    
+-- quicksort [] = []
+-- quicksort (x:xs) =
+--     let smallerSorted = quicksort (filter (<=x) xs)
+--         biggerSorted = quicksort (filter (>x) xs)
+--     in  smallerSorted ++ [x] ++ biggerSorted
